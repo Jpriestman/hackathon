5bb5ccc65d4ac5d2995df30d8b39dabec6afd8aa
Patel <a_patel73563@tss-hr420-dm05.fc.ca>
2018-10-10 14:33:23 -0400

    small screen coding done

---
+++ index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <header>
+      <div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
+        <button class="menu-icon columns" type="button" data-toggle="main-menu"></button>
+        <div class="title-bar-title columns text-center"><label id="projectName" class="columns small-12 centered">Agency X</label></div>
+      </div>
+
+      <nav id="main-menu" class="row">  
+          <h2 class="hidden" style="display:none">Navigation</h2>
+        <ul id="menu" class="menu vertical medium-horizontal text-center expanded columns small-12">
+          <li class="columns small-12"><a href="#home">HOME</a></li>
+          <li class="columns small-12"><a href="#about">ABOUT</a></li>
+          <li class="columns small-12"><a href="#work">WORK</a></li>
+          <li class="columns small-12"><a href="#contactus">CONTACT</a></li>
+        </ul>  
+      </nav>
+    </header>
+
+    <section class="row">
+      <h2 class="columns small-12">Title</h2>
+      <p class="columns small-12">text goes here</p>
+      <!-- buttons -->
+      <div class="columns small-10 small-centered">
+        <input type="button" class="columns small-4 small-offset-1 button" value="Register">
+        <input type="button" class="columns small-4 button small-offset-2" value="Discover">
+      </div>
+    </section>
+
+    <section class="row">
+      <h2 class="columns small-12  text-center">Working functions</h2>
+      <p class="columns small-12  text-center">Lorem dolor khjsfdjsdnjkns sf df </p>
+      <div class="columns small-10 small-centered">
+        <img src="" class="columns small-12" alt="promoimage">
+        <h3 class="columns small-12 text-center">Environment control system</h3>
+        <p class="columns small-12 text-center">Lorm iso d dgrd sdf d dfz</p>
+    </div>
+    </section>
+
+  <section class="row">
+    <h2 class="columns small-12 text-center"> How It Works</h2>
+    <p class="columns small-12 text-center">jhgab jhgb jh bjhg bjyg bh</p>
+    <img class="columns small-8 small-centered" src="" alt="mobileview">
+    <div class="row">
+      <img src="" class="columns small-2" alt="register">
+      <div class="columns small-10">
+        <h3 class="columns small-12">Register</h3>
+        <p class="columns small-12">dg gtd hgfg hf gh fcghfg </p>
+      </div>
+    </div>
+    <div class="row">
+      <img src="" class="columns small-2" alt="Setup">
+      <div class="columns small-10">
+        <h3 class="columns small-12">Set Up</h3>
+        <p class="columns small-12">dg gtd hgfg hf gh fcghfg </p>
+      </div>
+    </div>
+    <div class="row">
+      <img src="" class="columns small-2" alt="control">
+      <div class="columns small-10">
+        <h3 class="columns small-12">control</h3>
+        <p class="columns small-12">dg gtd hgfg hf gh fcghfg </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="row">
+    <h2 class="columns small-12 text-center">Easy To Manage</h2>
+    <p class="columns small-12">xfhcfg gf g xfg fgfgb fgb cgbcgb c ccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfg</p>
+    <input class="columns button small-3" value="Discover >">
+  </section>
+
+
+  <section class="row">
+    <h2 class="columns small-12 text-center">Use From Anywhere</h2>
+    <p class="columns small-12">xfhcfg gf g xfg fgfgb fgb cgbcgb c ccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfg</p>
+    <input class="columns button small-3" value="Discover >">
+  </section>
+
+  <section class="row">
+    <h2 class="columns small-12 text-center">What Our Clients Think?</h2>
+    <p class="columns small-12">xfhcfg gf g xfg fgfgb fgb cgbcgb c ccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfg</p>
+    <input class="columns button small-3" value="Discover >">
+  </section>
+
+  <section class="row">
+    <form>
+      <h2 class="columns small-12">Subscribe to Our Newsletter</h2>
+      <p class="columns small-12">gfgb fgb cgbcgb c ccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f hb ghbg    gh fg h fghf gh fg h
+      df gdf g gch g hfg hfh fg hfghfg hgfccx gbcgcbvcbcgbgc  g bg bhf g  g f</p>
+      <div class="columns small-12">
+        <input type="text" class="columns small-8" placeholder="Enter Your Email Address ....">
+        <input type="submit" class="columns small-4 button" value="Subscribe">
+      </div>
+    </form>
+  </section>
+
+  <footer>
+
+
+  </footer>
+<script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
